5ada80c50a354e5e450710f3dabac5da97394394
Eric Whitcomb <eric.whitcomb@gmail.com>
2020-01-31 21:54:49 -0700

    Set up axios and made connection to backend

---
+++ src/components/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import axios from 'axios';
+
+class HomePage extends React.Component {
+
+    componentDidMount() {
+        axios.get('/api')
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => console.log(error));
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Testing for connection to backend</h1>
+                <h2>Check console in dev tools</h2>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
