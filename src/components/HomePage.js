4489a7fcb75e7a18c66116128f74afc80ff8450c
Eric Whitcomb <eric.whitcomb@gmail.com>
2020-02-06 00:15:32 -0700

    Switched to using react-bootstrap
    Removed bootstrap, jquery, and popper.js modules

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,8 +2,16 @@ import React from 'react';
 import axios from 'axios';
 import image from '../images/herm_edwards.jpg';
 import SignInModal from './authentication/SignInModal';
+import { Navbar, Nav, Form, FormControl, Button, Modal, Container, Row, Col, Jumbotron, Image } from 'react-bootstrap';
 
 class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showModal: false
+        }
+    }
 
     componentDidMount() {
         axios.get('/api')
@@ -15,35 +23,46 @@ class HomePage extends React.Component {
 
     render() {
         return (
-            <div>
-                <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                    <a class="navbar-brand" href="#"><i class="fas fa-trophy"></i> Pro Fanatics</a>
-                </nav>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="jumbotron">
-                                <div class="row">
-                                    <div class="col-6 text-center">
-                                        <h1 class="display-3">Pro Fanatics</h1>
-                                        <hr class="my-4" />
-                                        <p class="lead">A goal without a plan is a wish.</p>
+            <>
+                {/* <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                    <a class="navbar-brand" href="/"><i class="fas fa-trophy"></i> Pro Fanatics</a>
+                </nav> */}
+                <Navbar variant="dark" expand="lg" bg="primary">
+                    <Navbar.Brand href="/"><i class="fas fa-trophy"></i> Pro Fanatics</Navbar.Brand>
+                </Navbar>
+                <Container>
+                    <Row>
+                        <Col xs="12">
+                            <Jumbotron>
+                                <Row>
+                                    <Col md="6" className="text-center">
+                                        <h1 className="display-3">Pro Fanatics</h1>
+                                        <hr className="my-4" />
+                                        <p className="lead">A goal without a plan is a wish.</p>
                                         <p>Herm Edwards</p>
-                                        <p class="lead">
-                                            <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">Sign In</button>
+                                        <p className="lead">
+                                            <Button variant="primary" onClick={this.showModal}>Sign In</Button>
                                         </p>
-                                    </div>
-                                    <div class="col-6">
-                                        <img src={image} class="img-fluid" alt="Herm Edwards" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <SignInModal />
-            </div>
+                                    </Col>
+                                    <Col md="6">
+                                        <Image src={image} rounded fluid />
+                                    </Col>
+                                </Row>
+                            </Jumbotron>
+                        </Col>
+                    </Row>
+                </Container>
+                <SignInModal show={this.state.showModal} onHide={this.hideModal} />
+            </>
         );
+    }
+
+    showModal = () => {
+        this.setState({ showModal: true });
+    }
+
+    hideModal = () => {
+        this.setState({ showModal: false })
     }
 }
 
--- src/components/authentication/SignInModal.js
+++ src/components/authentication/SignInModal.js
@@ -1,145 +1,123 @@
 import React from 'react';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
+import { Button, Modal } from 'react-bootstrap';
 
 class SignInModal extends React.Component {
-    state = {
-        email: '',
-        password: '',
-        message: '',
-        focus: 1,
-        error: 0
-    };
+    constructor(props) {
+        super(props);
 
-    InputHandler = event => {
-        event.preventDefault();
-        const target = event.target;
-        this.setState({
-            [target.name]: target.value,
-            error: 0,
-            message: ''
+        this.state = {
+            email: '',
+            password: '',
+            message: '',
+            focus: 1,
+            error: 0
+        };
+    }
+
+    // inputHandler = event => {
+    //     event.preventDefault();
+    //     console.log('inputHandler')
+    //     const target = event.target;
+    //     this.setState({
+    //         [target.name]: target.value,
+    //         error: 0,
+    //         message: ''
+    //     });
+    // };
+
+    // submitHandler = event => {
+    //     event.preventDefault();
+    //     console.log('submitHandler')
+
+    // const { email, password } = this.state;
+
+    // if (!email) {
+    //   this.setState({
+    //     message: 'Email cannot be empty',
+    //     focus: 1,
+    //     error: 1
+    //   });
+    // } else if (!password) {
+    //   this.setState({
+    //     message: 'Password cannot be empty',
+    //     focus: 2,
+    //     error: 2
+    //   });
+    // } else {
+    //   const endpoint = '/auth/login';
+    //   const credentials = { email, password };
+    //   axios
+    //     .post(endpoint, credentials)
+    //     .then(res => {
+    //       localStorage.setItem('jwt', res.data.token);
+    //       this.context.signin();
+    //       this.setState({ email: '', password: '', signedIn: true });
+    //     })
+    //     .catch(err => {
+    //       this.setState({
+    //         message: 'Email or Password is incorrect'
+    //       });
+    //     });
+    // }
+    // };
+
+    componentDidMount() {
+        console.log('componentDidMount()');
+        window.addEventListener('shown.bs.modal', (e) => {
+            console.log('shown.bs.modal');
         });
-    };
+    }
 
-    SubmitHandler = event => {
-        event.preventDefault();
-
-        // const { email, password } = this.state;
-
-        // if (!email) {
-        //   this.setState({
-        //     message: 'Email cannot be empty',
-        //     focus: 1,
-        //     error: 1
-        //   });
-        // } else if (!password) {
-        //   this.setState({
-        //     message: 'Password cannot be empty',
-        //     focus: 2,
-        //     error: 2
-        //   });
-        // } else {
-        //   const endpoint = '/auth/login';
-        //   const credentials = { email, password };
-        //   axios
-        //     .post(endpoint, credentials)
-        //     .then(res => {
-        //       localStorage.setItem('jwt', res.data.token);
-        //       this.context.signin();
-        //       this.setState({ email: '', password: '', signedIn: true });
-        //     })
-        //     .catch(err => {
-        //       this.setState({
-        //         message: 'Email or Password is incorrect'
-        //       });
-        //     });
-        // }
-    };
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate()');
+    }
 
     render() {
+        console.log('render()');
         return (
-            //   <main className={classes.main}>
-            //     <Navbar />
-            //     <CssBaseline />
-            //     <Paper className={classes.paper}>
-            //       <Avatar className={classes.avatar}>
-            //         <LockOutlinedIcon />
-            //       </Avatar>
-            //       <Typography component="h1" variant="h5">
-            //         Sign in
-            //       </Typography>
-            //       <p className={classes.message}>{this.state.message}</p>
-            //       <form className={classes.form} onSubmit={this.SubmitHandler}>
-            //         <FormControl 
-            //             margin="normal" 
-            //             // required
-            //             fullWidth
-            //             error={this.state.error === 1 ? true : false}>
-            //           <InputLabel htmlFor="email">Email Address</InputLabel>
-            //           <Input
-            //             id="email"
-            //             name="email"
-            //             onChange={this.InputHandler}
-            //             autoComplete="email"
-            //             autoFocus
-            //           />
-            //         </FormControl>
-            //         <FormControl
-            //             margin="normal" 
-            //             // required
-            //             fullWidth
-            //             error={this.state.error === 2 ? true : false}>
-            //           <InputLabel htmlFor="password">Password</InputLabel>
-            //           <Input
-            //             id="password"
-            //             name="password"
-            //             onChange={this.InputHandler}
-            //             type="password"
-            //           />
-            //         </FormControl>
-            //         <FormControlLabel
-            //           control={<Checkbox value="remember" color="primary" />}
-            //           label="Remember me"
-            //         />
-            //         <Button
-            //           type="submit"
-            //           fullWidth
-            //           variant="contained"
-            //           color="primary"
-            //           className={classes.submit}
-            //         >
-            //           Sign in
-            //         </Button>
-            //       </form>
-            //       <a
-            //         href={process.env.REACT_APP_API_URL + '/auth/google'}
-            //         variant="contained"
-            //         color="primary"
-            //         className={classes.submit}
-            //       >
-            //         Login with Google
-            //       </a>
-            //     </Paper>
-            //   </main>
-            <div id="myModal" class="modal" role="dialog">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title">Modal title</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <p>Modal body text goes here.</p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            // <div id="myModal" class="modal" role="dialog" ref={this.myRef}>
+            //     <div class="modal-dialog" role="document">
+            //     <div class="modal-content">
+            //         <form onSubmit={this.submitHandler}>
+            //             <div class="modal-header">
+            //                 <h5 class="modal-title">Pro Fanatics Sign In</h5>
+            //                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            //                     <span aria-hidden="true">&times;</span>
+            //                 </button>
+            //             </div>
+            //             <div class="modal-body">
+            //                 <div class="form-group row">
+            //                     <label for="email" class="col-sm-2 col-form-label">Email</label>
+            //                     <div class="col-sm-10">
+            //                         <input type="email" class="form-control" id="email" onChange={this.inputHandler} />
+            //                     </div>
+            //                 </div>
+            //                 <div class="form-group row">
+            //                     <label for="password" class="col-sm-2 col-form-label">Password</label>
+            //                     <div class="col-sm-10">
+            //                         <input type="password" class="form-control" id="password" onChange={this.inputHandler} />
+            //                     </div>
+            //                 </div>
+            //             </div>
+            //             <div class="modal-footer">
+            //                 <button type="submit" class="btn btn-primary btn-lg btn-block">Sign In</button>
+            //             </div>
+            //         </form>
+            //     </div>
+            // </div>
+            // </div>
+
+              <Modal show={this.props.show} onHide={this.props.onHide}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Pro Fanatics</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                  <Button variant="primary">Sign In</Button>
+                </Modal.Footer>
+              </Modal>
         );
     }
 }
