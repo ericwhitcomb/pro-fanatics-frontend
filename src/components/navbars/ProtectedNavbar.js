c517fb21b71d359c1bfef8151f3d586d30822153
Eric Whitcomb <eric.whitcomb@gmail.com>
2020-02-10 22:58:00 -0700

    Add sign out

---
+++ src/components/navbars/ProtectedNavbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+
+function ProtectedNavbar() {
+    return (
+        <>
+            <Navbar variant="dark" expand="lg" bg="primary">
+                <Navbar.Brand href="/"><i class="fas fa-trophy"></i> Pro Fanatics</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                    <Nav>
+                        <Nav.Item>
+                            <Nav.Link href="/signout">Sign Out</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    );
+}
+
+export default ProtectedNavbar;
