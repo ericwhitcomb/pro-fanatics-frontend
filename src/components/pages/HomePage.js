5e2a59f30ad252754fdc95581b7f7cc884a6c9a4
Eric Whitcomb <eric.whitcomb@gmail.com>
2020-02-12 22:56:15 -0700

    Fix warning for improper JSX attribute. class to className

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -25,7 +25,7 @@ class HomePage extends React.Component {
         return (
             <>
                 <Navbar variant="dark" expand="lg" bg="primary">
-                    <Navbar.Brand href="/"><i class="fas fa-trophy"></i> Pro Fanatics</Navbar.Brand>
+                    <Navbar.Brand href="/"><i className="fas fa-trophy"></i> Pro Fanatics</Navbar.Brand>
                 </Navbar>
                 <Container>
                     <Row>
