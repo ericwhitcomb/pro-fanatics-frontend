8f9635fc1560954bfa590f7f2abbc6fdfa16a902
Eric Whitcomb <eric.whitcomb@gmail.com>
2020-02-12 22:40:49 -0700

    Remove console logs from debugging session

--- src/index.js
+++ src/index.js
@@ -8,12 +8,6 @@ import App from './App';
 
 dotenv.config();
 
-console.log('index.js:' + process.env.NODE_ENV);
-
-console.log('index.js:' + process.env.REACT_APP_BACKEND_URL);
-
 axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
-
-console.log('index.js:' + axios.defaults.baseURL);
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
